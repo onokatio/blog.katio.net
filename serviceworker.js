b13dd83a44791fcd5de5361af8e594a8b20f4382
onokatio <onokatio@gmail.com>
2019-11-01 00:35:31 +0900

    full cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,12 +1,28 @@
+const cacheList = [
+	'/',
+	'/main.css',
+	'/index.js',
+	'/favicon.ico'
+	'https://cdn.honokak.osaka/honoka/4.3.1/css/bootstrap.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/solarized-dark.min.css',
+	'https://cdnjs.cloudflare.com/ajax/libs/marked/0.6.1/marked.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/highlight.min.js',
+	'https://cdn.jsdelivr.net/npm/emoji-toolkit@5.0.5/lib/js/joypixels.min.js',
+	'https://cdn.jsdelivr.net/npm/emoji-toolkit@5.0.5/extras/css/joypixels.min.css',
+	'https://unpkg.com/js-yaml@3.10.0/dist/js-yaml.js',
+	'https://dworthen.github.io/js-yaml-front-matter/js/yamlFront.js'
+]
+
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open('v1')
-			.then( cache => cache.addAll(['/', '/main.css', 'index.js', 'favicon.ico' ]) )
+			.then( cache => cache.addAll(cacheList) )
 	)
 })
 
 self.addEventListener('fetch', event => {
-	console.log('sw: fetch event triggered.');
+	console.log('sw: fetch triggered.');
 	event.respondWith(
 		caches.match(event.request)
 			.then( response => {
