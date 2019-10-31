bf7a6446752bdff70945bd0cb80632a2b654aa96
onokatio <onokatio@gmail.com>
2019-10-31 23:39:55 +0900

    sw fetch event

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,21 @@
 self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open('v1')
-			.then( cache => cache.addAll(['/', '/main.css', 'index.js' ]) )
+			.then( cache => cache.addAll(['/', '/main.css', 'index.js', 'favicon.ico' ]) )
+	)
+})
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+			.then( response => {
+				return response || fetch(event.request).then( response2 => {
+					caches.open('v1')
+						.then( cache => {
+							cache.put(event.request, response2.clone())
+							return response;
+						})
+				})
+			} )
 	)
 })
