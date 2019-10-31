cc4d55f0b9748a8ac18712cfa2986ea2f77c4a75
onokatio <onokatio@gmail.com>
2019-10-31 23:54:50 +0900

    add log and fix

--- serviceworker.js
+++ serviceworker.js
@@ -6,14 +6,16 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
+	console.log('sw: fetch event triggered.');
 	event.respondWith(
 		caches.match(event.request)
 			.then( response => {
 				return response || fetch(event.request).then( response2 => {
-					caches.open('v1')
+					console.log('sw: fetching non cached resources')
+					return caches.open('v1')
 						.then( cache => {
 							cache.put(event.request, response2.clone())
-							return response;
+							return response2;
 						})
 				})
 			} )
