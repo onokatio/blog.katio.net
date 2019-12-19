2627a92bebbc5f638af2f31ecfc537e733b69a03
onokatio <onokatio@gmail.com>
2019-12-19 18:22:59 +0900

    use highlight.js npm package

--- src/RenderMarkdown.js
+++ src/RenderMarkdown.js
@@ -2,6 +2,8 @@
 
 import marked from 'marked';
 import hljs from 'highlight.js';
+import 'highlight.js/styles/solarized-dark.css'
+
 const yamlFront = require("yaml-front-matter")
 
 marked.setOptions({
