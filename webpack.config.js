5e339e2fa8b83b0e5234ef1e89ff887c877b4309
onokatio <onokatio@gmail.com>
2019-12-19 17:38:32 +0900

    rename dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
+const path = require('path');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    ecmaVersion: 2015,
-  }
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.scss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	resolve: { extensions: ["*", ".js", ".jsx"] },
+	devServer: {
+		contentBase: path.join(__dirname, 'public/'),
+		watchContentBase: true,
+	},
+	output: {
+		ecmaVersion: 2015,
+		filename: 'main.js',
+		path: path.join(__dirname, 'public/'),
+	}
 };
